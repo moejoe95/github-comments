1b3226cecfa3fcbda5d70a83ba0eb7bc48c46370
moejoe95 <j.gugglberger@gmail.com>
2020-01-15 20:43:39 +0100

    add pagination to request that gets top repos

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ import argparse
 import os
 import requests
 import shutil
+import pandas as pd
+import OAuth
 
 from extractor import CommentExtractor
 from analyzer import Analyzer
 
 lang2ending =  {'Python': 'py', 'Java': 'java'}
+git_url = 'https://github.com/'
 
 
 def get_top_repos(lang, n):
-    top_repo_req = requests.get('https://api.github.com/search/repositories?q=language:' + lang + '&sort:stars&sort=desc')
-    top_repos_json = top_repo_req.json()
+    page_num = n // 30 + 2
+
+    repo_pages = []
+    for i in range(1, page_num):
+        top_repo_req = requests.get('https://api.github.com/search/repositories?q=language:' 
+            + lang + '&sort:stars&sort=desc&page=' + str(i), 
+            headers={'Authorization': OAuth.TOKEN})
+        top_repos_json = top_repo_req.json()
+        repo_pages.append(top_repos_json['items'])
 
     top_repos = dict()
-    for i, repo in enumerate(top_repos_json['items']):
-        top_repos.update({repo['full_name']: lang2ending.get(lang)})
-        if i+1 == n:
-            break
+    i = 0
+    for repos in repo_pages:
+        for repo in repos:
+            top_repos.update({repo['full_name']: lang2ending.get(lang)})
+            if i+1 == n:
+                return top_repos
+            i += 1
 
     return top_repos
 
@@ -46,15 +59,19 @@ else:
 for repo_path, lang in repos.items():
     out = args.out
 
-    git_url = 'https://github.com/'
+    df = pd.read_csv('dataframe.csv', index_col=False)
+    if df['project'].isin([repo_path]).any():
+        print('repo', repo_path, 'is already in dataframe, continue ...')
+        continue
 
     if os.path.isdir(repo_path):
-        print('repo', repo_path, 'already exists')
+        print('repo', repo_path, 'already exists in current folder')
     else:
+        continue
         print('cloning repo into: ' + repo_path)
         Repo.clone_from(git_url + repo_path, repo_path)
 
-    print('collecting comments in ', repo_path, '\n')
+    print('collecting comments ...')
 
     # traverse files in repo and collect comments
     extr = CommentExtractor(lang, repo_path)
@@ -64,7 +81,8 @@ for repo_path, lang in repos.items():
     if out:
         extr.write_files(out)
 
-    req_repo = requests.get('https://api.github.com/repos/' + repo_path)
+    req_repo = requests.get('https://api.github.com/repos/' + repo_path, 
+        headers={'Authorization': OAuth.TOKEN})
     meta_data_repo = req_repo.json()
 
     an = Analyzer()
@@ -72,4 +90,5 @@ for repo_path, lang in repos.items():
     #an.print_dataframe()
 
     if args.rm:
+        print('removing repo', repo_path, '...\n')
         shutil.rmtree(repo_path.split('/')[0])
